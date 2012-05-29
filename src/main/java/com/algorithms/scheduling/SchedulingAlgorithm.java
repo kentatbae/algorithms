6a690a399effed586036af0efd385fd61abde494
Kent McHenry <kentatbae@gmail.com>
2012-05-29 02:07:19 -0700

    updating javadoc

--- src/main/java/com/algorithms/scheduling/SchedulingAlgorithm.java
+++ src/main/java/com/algorithms/scheduling/SchedulingAlgorithm.java
@@ -9,6 +9,16 @@ import java.util.Set;
  * @author Kent McHenry
  */
 public interface SchedulingAlgorithm {
-
-  public List<TimeInterval> accept(Set<TimeInterval> interval);
+  
+  /**
+   * Method for selecting items for <tt>intervals</tt>. Any algorithm should
+   * select only a non-intersecting subset of the items contains within
+   * <tt>intervals</tt>.
+   * 
+   * @param interval
+   *          A set of {@link TimeInterval}s to select from
+   * @return an ordered list of (non-intersecting) accepted {@link TimeInterval}
+   *         s.
+   */
+  public List<TimeInterval> accept(Set<TimeInterval> intervals);
 }
